30ea0f9a4f1460e4484ad9d3bcd3271d65ed8d67
Distractic <distractic@outlook.fr>
2022-08-02 20:58:01 +0200

    tests: Add Profile Skin tests

---
+++ src/jvmTest/kotlin/io/github/universeproject/utils/Generator.kt
@@ -0,0 +1,18 @@
+package io.github.universeproject.utils
+
+import java.util.*
+
+val stringGenerator = generateSequence { UUID.randomUUID().toString() }.distinct().iterator()
+
+fun getRandomString() = stringGenerator.next()
+
+fun generateRandomNameWithInvalidSymbol() = generateRandomName().drop(1) + "&"
+
+fun generateRandomName(): String {
+    val validCharRanges: List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+    return generateSequence { validCharRanges.random() }.take(16).joinToString("")
+}
+
+fun generateUUIDWithInvalidSymbol() = generateRandomUUID().drop(1) + "&"
+
+fun generateRandomUUID() = getRandomString()
